49ff2ea2ef20a8a098c148b75eebb01383896168
carolin.labinski <carolin.labinski@yahoo.de>
2020-02-27 23:17:17 +0100

    faktor

--- script_2.js
+++ script_2.js
@@ -1,25 +1,39 @@
-var nombre = prompt("De quel nombre veux-tu calculer la factorielle ?");
-console.log("Nombre choisi:" + " " + nombre);
-
-function factorial(nombre) 
-
-{ 
-
-  if (nombre === 0)
- {
-    return 1;
- }
-  return nombre * factorial(nombre-1);
-         
+let nbr = prompt("De quel nombre veux-tu calculer la factorielle ?");
+if (nbr == 0 || nbr == 1){
+  console.log ("Le résultat est : 1")
 }
-console.log("Le résultat est:" + " " + factorial(nombre));
+else{
+  let i = nbr;
+    while(nbr > 1){
+      nbr--
+      i = nbr * i
+    }
+  let answer = `Le résultat est : ${i}`
+  console.log(answer)
 
 
-// if(nombre == 0 || nombre == 1) {
-// console.log("Le résultat est : 1");
-// } else {
-// let result = factorial(nombre-1) * nombre 
-// console.log(result);
+
+
+
+
+
+// var nombre = prompt("De quel nombre veux-tu calculer la factorielle ?");
+// console.log("Nombre choisi:" + " " + nombre);
+
+// function factorial(nombre) 
+
+// { 
+
+//   if (nombre === 0)
+//  {
+//     return 1;
+//  }
+//   return nombre * factorial(nombre-1);
+         
 // }
+// console.log("Le résultat est:" + " " + factorial(nombre));
+
+
+
 
 
